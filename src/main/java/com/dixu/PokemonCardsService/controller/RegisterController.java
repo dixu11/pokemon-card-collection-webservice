12ee50e8c77628af1184e7caa20c77dac65698fc
Dixu <you.are.imba@gmail.com>
2020-05-10 12:51:23 +0200

    connect form to server

--- src/main/java/com/dixu/PokemonCardsService/controller/RegisterController.java
+++ src/main/java/com/dixu/PokemonCardsService/controller/RegisterController.java
@@ -2,12 +2,23 @@ package com.dixu.PokemonCardsService.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/register")
 class RegisterController {
-    @GetMapping("/register")
+    @GetMapping
     String getRegisterPage(){
         return "register-form";
+    }
+
+    @PostMapping
+    String registerNewUser(@RequestParam String mail, @RequestParam String pass) {
+        System.out.println(mail);
+        System.out.println(pass);
+        return "index";
     }
 
 }
---
+++ src/main/java/com/dixu/PokemonCardsService/user/User.java
@@ -0,0 +1,9 @@
+package com.dixu.PokemonCardsService.user;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String mail;
+    private String password;
+}
