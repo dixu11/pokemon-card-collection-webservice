4e5c84f66e964bf6c85e76cce77201bfb9aaaa5d
Dixu <you.are.imba@gmail.com>
2020-05-10 12:05:17 +0200

    init project

---
+++ src/main/java/com/dixu/PokemonCardsService/PokemonCardsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dixu.PokemonCardsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonCardsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonCardsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dixu/PokemonCardsService/PokemonCardsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dixu.PokemonCardsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonCardsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
